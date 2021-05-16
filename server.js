9681750ba1940d52189125bbb4f33ed8b89573a1
KunalMohadikar <kunalmohadikar44@gmail.com>
2021-05-16 20:42:45 +0300

    changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covid-resource-angular'));
+app.use(express.static(__dirname + '/dist/COVID_Resources'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/covid-resource-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/COVID_Resources/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
