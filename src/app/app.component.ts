00aa0c3f9f105017826311dc7d0aafd6f8909601
KunalMohadikar <kunalmohadikar44@gmail.com>
2021-05-18 15:15:00 +0300

    added time column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { DataElement } from './common/datasource';
 import { ConfigService } from './config.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Users } from './common/users';
+import { DateHelper } from './common/datehelper';
 
 @Component({
   selector: 'app-root',
@@ -43,26 +44,7 @@ export class AppComponent {
   res = {name: "AA"};
   resource = '';
   location = '';
-  displayedColumns = ['full_text', "phoneNo"]
-
-
-  dataSource: Array<DataElement> = [
-    {
-      created_at: 1,
-      full_text: "Hello Brother",
-      phoneNo: 2222222222
-    },
-    {
-      created_at: 2,
-      full_text: "Hello Brother 2",
-      phoneNo: 1111111111
-    },
-    {
-      created_at: 3,
-      full_text: "Hello Brother 3",
-      phoneNo: 3333333333
-    },
-  ];
+  displayedColumns = ['created_at','full_text', "phoneNo"]
 
 
   getUsers()
@@ -87,6 +69,31 @@ export class AppComponent {
           let created2Date = new Date(created2.replace("+0000 ", "") + " UTC");
           return created1Date < created2Date ? 1 : -1;
         });
+        response.forEach(function (elem) {
+          let created1 = elem.created_at;
+          let created1Date = new Date(created1.replace("+0000 ", "") + " UTC");
+          let currentDate = new Date();
+          const diffTime = Math.abs(currentDate.getTime() - created1Date.getTime());
+          elem.timeDiffSeconds = diffTime;
+          elem.timeDiffMins = Math.ceil(diffTime/(1000*60));
+          let formatted = "";
+          let dateHelper = new DateHelper();
+          if(elem.timeDiffMins > 1339){
+              const monthName = dateHelper.months[created1Date.getMonth()];
+              const year = created1Date.getFullYear();
+              const date = created1Date.getDate();
+              formatted = `${date} ${monthName} ${year}`;
+          }
+          else if(elem.timeDiffMins > 59){
+              const hours = Math.floor(elem.timeDiffMins/60);
+              const mins = elem.timeDiffMins%60;
+              formatted = `${hours} hr, ${mins} mins ago`;
+          }
+          else{
+              formatted = elem.timeDiffMins.toString() + " mins ago";
+          }
+          elem.dateTime = formatted;
+        })
         this.users = response;
         console.log(this.users[0]);
         this.isData = true;
--- src/app/common/users.ts
+++ src/app/common/users.ts
@@ -1,8 +1,13 @@
+import { DateHelper } from "./datehelper";
+
 export class Users
 {
     created_at: string;
     full_text:string;
     phoneNo: Array<string>;
+    timeDiffSeconds: number;
+    timeDiffMins: number;
+    dateTime: string;
 
 
     constructor(created_at: string, full_text: string, phoneNo: Array<string>)
@@ -10,6 +15,30 @@ export class Users
         this.created_at = created_at;
         this.full_text = full_text;
         this.phoneNo = phoneNo;
+        let created1 = created_at;
+        let created1Date = new Date(created1.replace("+0000 ", "") + " UTC");
+        let currentDate = new Date();
+        const diffTime = Math.abs(currentDate.getTime() - created1Date.getTime());
+        this.timeDiffSeconds = diffTime;
+        this.timeDiffMins = Math.ceil(diffTime/(1000*60));
+        let formatted = "";
+        let dateHelper = new DateHelper();
+        if(this.timeDiffMins > 1339){
+            const monthName = dateHelper.months[created1Date.getMonth()];
+            const year = created1Date.getFullYear();
+            const date = created1Date.getDate();
+            formatted = `${date} ${monthName} ${year}`;
+        }
+        else if(this.timeDiffMins > 59){
+            const hours = Math.floor(this.timeDiffMins/60);
+            const mins = this.timeDiffMins%60;
+            formatted = `${hours} hrs, ${mins} mins ago`;
+
+        }
+        else{
+            formatted = this.timeDiffMins.toString() + " mins ago";
+        }
+        this.dateTime = formatted;
     }
 
 
