4d351275c59ee31deee0a5634ec8f811c3519400
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-25 16:15:05 +0530

    input

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -2,23 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Users } from './common/users';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfigService {
+  // url : string = "http://localhost:3000/Users/"
+  url: string = 'https://covid-resource-express.herokuapp.com/users/';
 
-  url : string = "http://localhost:3000/Users/"
-  // url : string = 'https://covid-resource-express.herokuapp.com/users/'
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) {}
-
-  
-
-
-  getUsers()
-  {
-    console.log("returning http object");
+  getUsers() {
+    console.log('returning http object');
     // const params = new HttpParams()
     // .set('location', this.location);
     return this.http.get<Users[]>(this.url);
